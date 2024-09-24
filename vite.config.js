74403c142177af2386c16f4dfc47ad0efd890653
Bubble <bubble7671@gmail.com>
2024-09-24 15:55:16 +0800

    build: 更新 build 配置
    (cherry picked from commit d8386a4dbb91a847d954a05ecc18f3c9d94615a0)

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,33 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import * as path from 'path';
 
+// 当前工作目录路径
+const root = process.cwd();
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: false,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  // 环境变量
+  const env = loadEnv(mode, root, "");
+  return {
+    base: env.VITE_BASE_URL || "/",
+    plugins: [react()],
+    build: {
+      sourcemap: false,
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-  },
-  test: {
-    css: false,
-    include: ['src/**/__tests__/*'],
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.ts',
-    clearMocks: true,
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    },
+    test: {
+      css: false,
+      include: ['src/**/__tests__/*'],
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: 'src/setupTests.ts',
+      clearMocks: true,
+    },
+  }
 });
